f51bb10d6334d1c50bca31d10916ddf2326b5492
William <will2144@icloud.com>
2023-05-04 11:43:38 -0400

    Finished Main Class

--- src/main/java/com/learntocode/Main.java
+++ src/main/java/com/learntocode/Main.java
@@ -1,4 +1,11 @@
 package com.learntocode;
 
 public class Main {
+    public static void main(String[] args) {
+        TransactionManager transactionManager = new TransactionManager();
+        LedgerScreen ledgerScreen = new LedgerScreen(transactionManager);
+        HomeScreen homeScreen = new HomeScreen(transactionManager, ledgerScreen);
+
+        homeScreen.show();
+    }
 }
